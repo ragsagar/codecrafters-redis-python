48414331f1112ecb6b8d988a31f86d0c78dcd65b
Rag Sagar <ragsagar@gmail.com>
2024-05-26 20:45:30 +0400

    Update

--- app/encoder.py
+++ app/encoder.py
@@ -20,5 +20,13 @@ class Encoder:
     def generate_simple_string(self, message):
         return f"+{message}\r\n".encode()
 
-    def generate_file_string(self, contents):
-        return f"${len(contents)}\r\n{contents}".encode()
+    def generate_file_string(self, hex_contents):
+        result_string = "".join(
+            [
+                chr(int(hex_contents[i : i + 2], 16))
+                for i in range(0, len(hex_contents), 2)
+            ]
+        )
+        byte_contents = bytes.fromhex(hex_contents)
+        print("File contents", hex_contents)
+        return f"${len(result_string)}\r\n{result_string}".encode()
--- app/server.py
+++ app/server.py
@@ -167,7 +167,7 @@ class RedisServer:
     def get_rdb_file_contents(self):
         # hex_data = open("./sample_file.rdb").read()
         hex_data = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
-        return bytes.fromhex(hex_data)
+        return hex_data
 
     def initialize_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
