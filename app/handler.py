7b355d557d789cd4aed9466012f39e0109f75bcb
Rag Sagar <ragsagar@gmail.com>
2024-05-27 20:30:20 +0400

    Update

--- app/handler.py
+++ app/handler.py
@@ -88,8 +88,8 @@ class CommandHandler:
         parts = message.strip().split(b"\r\n")
         print("Parsing message", message, "parts", parts)
         commands = []
-        if parts[0] == "+":
-            return [parts[1:].decode()]
+        if parts[0] == "+OK":
+            return None
         elif parts[0] == "$":
             length = int(parts[0][1:])
             for i in range(length):
