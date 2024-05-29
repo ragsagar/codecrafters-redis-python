727e9fbcf869b65cb27259a32ac4fd1670b53b22
Rag Sagar <ragsagar@gmail.com>
2024-05-29 10:01:02 +0400

    Update

---
+++ app/store.py
@@ -0,0 +1,24 @@
+import datetime
+
+
+class KeyValueStore:
+    def __init__(self):
+        self.data = {}
+
+    def get(self, key):
+        return self.data.get(key)
+
+    def set(self, key, value, expiry_milliseconds=None):
+        expiry_time = datetime.datetime.now() + datetime.timedelta(
+            milliseconds=expiry_milliseconds
+        )
+        self.data[key] = {
+            "value": value,
+            "expiry_time": expiry_time,
+        }
+
+    def expire_data(self):
+        print("Expiring data in store")
+        for key, value in self.data.items():
+            if value["expiry_time"] and value["expiry_time"] < datetime.datetime.now():
+                del self.data[key]
