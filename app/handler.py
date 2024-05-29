a19b9c62a9316f3122ea351b5f9ce7b08f8fc41d
Rag Sagar <ragsagar@gmail.com>
2024-05-29 20:35:39 +0400

    Update

--- app/handler.py
+++ app/handler.py
@@ -1,23 +1,24 @@
-import datetime
+from .parser import RespParser
 
 
 class CommandHandler:
     def __init__(self, server):
         self.server = server
+        self.parser = RespParser()
 
-    def _handle_set_command(self, data, incoming, sock):
-        key = incoming[1]
-        value = incoming[2]
+    def _handle_set_command(self, data, cmd, sock):
+        key = cmd.data[0]
+        value = cmd.data[1]
         expiry_milliseconds = None
-        if len(incoming) > 4:
-            expiry_command = incoming[3]
+        if len(cmd.data) > 2:
+            expiry_command = cmd.data[2]
             if expiry_command.upper() == "PX":
-                expiry_milliseconds = int(incoming[4])
+                expiry_milliseconds = int(cmd.data[3])
         self.server.set_data(key, value, expiry_milliseconds)
         return self.server.encoder.generate_success_string()
 
-    def _handle_get_command(self, data, incoming, sock):
-        key = incoming[1]
+    def _handle_get_command(self, data, cmd, sock):
+        key = cmd.data[0]
         value = self.server.get_data(key)
         if value:
             response_msg = self.server.encoder.generate_bulkstring(value)
@@ -25,7 +26,7 @@ class CommandHandler:
             response_msg = self.server.encoder.generate_null_string()
         return response_msg
 
-    def handle_replication_command(self, data, incoming, sock):
+    def handle_replication_command(self, data, cmd, sock):
         server_type = self.server.get_server_type()
         messages = [
             f"role:{server_type.value}",
@@ -41,30 +42,30 @@ class CommandHandler:
         self.server.log("Sending replication info", response_msg)
         return response_msg
 
-    def _handle_info_command(self, data, incoming, sock):
+    def _handle_info_command(self, data, cmd, sock):
         if incoming[1].upper() == "REPLICATION":
-            response_msg = self.handle_replication_command(data, incoming, sock)
+            response_msg = self.handle_replication_command(data, cmd, sock)
         else:
             response_msg = self.server.encoder.generate_bulkstring(
                 "redis_version:0.0.1"
             )
         return response_msg
 
-    def _handle_echo_command(self, data, incoming, sock):
-        echo_message = incoming[1]
+    def _handle_echo_command(self, data, cmd, sock):
+        echo_message = " ".join(cmd.data)
         response_msg = self.server.encoder.generate_bulkstring(echo_message)
         return response_msg
 
-    def _handle_ping_command(self, data, incoming, sock):
+    def _handle_ping_command(self, data, cmd, sock):
         return self.server.encoder.generate_bulkstring("PONG")
 
-    def _handle_replconf_command(self, data, incoming, sock):
-        print("Received replconf command", incoming)
+    def _handle_replconf_command(self, data, cmd, sock):
+        print("Received replconf command", cmd)
         return self.server.encoder.generate_success_string()
 
-    def _handle_psync_command(self, data, incoming, sock):
-        print(f"Received psync command", incoming)
-        self.server.add_replica(data.addr, incoming[1], incoming[2], sock)
+    def _handle_psync_command(self, data, cmd, sock):
+        print("Received psync command", cmd)
+        self.server.add_replica(data.addr, cmd.data[0], cmd.data[1], sock)
         resync_string = (
             f"FULLRESYNC {self.server.get_replid()} {self.server.get_repl_offset()}"
         )
@@ -74,31 +75,57 @@ class CommandHandler:
         )
         return resync_message + file_message
 
-    def _handle_ok_command(self, data, incoming, sock):
+    def _handle_ok_command(self, data, cmd, sock):
         return None
 
     def parse_message(self, message):
-        parts = message.strip().split(b"\r\n")
-        print("Parsing message", message, "parts", parts)
-        commands = []
-        if len(parts) == 1:
-            return [parts[0].decode()]
-        else:
-            length = int(parts[0][1:])
-            for i in range(length):
-                commands.append(parts[i * 2 + 2].decode())
+        commands = self.parser.parse(message)
         return commands
 
-    def handle_command(self, data, socket):
-        incoming = self.parse_message(data.outb)
-        print("Incoming data", incoming)
-        if (len(incoming) == 1 and incoming[0] == "OK") or not incoming:
-            return None
-        print("Incoming", incoming)
-        command = incoming[0].lower()
-        handler_func = getattr(self, f"_handle_{command}_command", None)
+    def handle_single_command(self, data, command):
+        handler_func = getattr(self, f"_handle_{command.command.lower()}_command", None)
         if not handler_func:
             response_msg = self.server.encoder.generate_bulkstring("Unknown command")
         else:
-            response_msg = handler_func(data, incoming, socket)
+            response_msg = handler_func(data, command)
         return response_msg
+
+    def handle_message(self, data):
+        commands = self.parse_message(data.outb)
+        response_msg = b""
+        for command in commands:
+            response = self.handle_single_command(data, command)
+            if response:
+                response_msg += response
+        return response_msg
+
+
+def run_tests():
+    test_handling()
+
+
+def test_handling():
+    handler = CommandHandler(None)
+    msg1 = b"*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n"
+    res1 = handler.handle_message(msg1)
+    assert res1 == b"+OK\r\n"
+
+    msg2 = b"*3\r\n$3\r\nSET\r\n$3\r\nbar\r\n$3\r\n456\r\n*3\r\n$3\r\nSET\r\n$3\r\nbaz\r\n$3\r\n789\r\n"
+    res2 = handler.parse_message(msg2)
+    print("Res2", res2)
+    assert res2 == b"+OK\r\n+OK\r\n"
+
+    msg3 = b"*1\r\n$4\r\nPING\r\n"
+    res3 = handler.handle_message(msg3)
+    assert res3 == b"+PONG\r\n"
+
+    msg4 = b"+OK\r\n"
+    res4 = handler.handle_message(msg3)
+    assert res4 == None
+
+    print("Parse message tests passed")
+
+
+if __name__ == "__main__":
+    run_tests()
+    print("All tests passed")
---
+++ app/parser.py
@@ -0,0 +1,108 @@
+from enum import Enum
+
+
+class RespParser:
+    class State:
+        START = 0
+        COMMAND = 1
+        DATA = 2
+        DONE = 3
+
+    def __init__(self) -> None:
+        self.state = self.State.START
+
+    def parse(self, message):
+        parts = message.split(b"\r\n")
+        message_length = 0
+        command = None
+        commands = []
+        for part in parts:
+            if message_length < 0:
+                raise ValueError("Invalid message", parts)
+            if part.startswith(b"$"):
+                # byte length
+                continue
+            if self.state == self.State.START:
+                if part.startswith(b"*"):
+                    message_length = int(part[1:])
+                    self.state = self.State.COMMAND
+                elif part.startswith(b"+"):
+                    command = Command(part[1:].decode())
+                    commands.append(command)
+                    self.state = self.State.START
+                    command = None
+            elif self.state == self.State.COMMAND:
+                message_length -= 1
+                command = Command(part.decode())
+                self.state = self.State.DATA
+            elif self.state == self.State.DATA:
+                message_length -= 1
+                command.add_data(part.decode())
+                if message_length == 1:
+                    self.state = self.State.DONE
+            elif self.state == self.State.DONE:
+                command.add_data(part.decode())
+                commands.append(command)
+                self.state = self.State.START
+                command = None
+        return commands
+
+
+class CommandType(Enum):
+    SET = "SET"
+    GET = "GET"
+    EXPIRE = "EXPIRE"
+    DELETE = "DELETE"
+    PING = "PING"
+    REPLCONF = "REPLCONF"
+    PSYNC = "PSYNC"
+    OK = "OK"
+
+
+class Command:
+    data = None
+
+    def __init__(self, command, data=None):
+        self.command = command
+        if data:
+            self.data = data
+        else:
+            self.data = []
+
+    def add_data(self, data):
+        self.data.append(data)
+
+    def __str__(self):
+        return f"{self.command} {self.data}"
+
+    def __repr__(self):
+        return f"{self.command} {self.data}"
+
+    def __eq__(self, other):
+        return self.command == other.command and self.data == other.data
+
+
+def run_tests():
+    print("Running tests")
+    parser = RespParser()
+    msg1 = b"*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n"
+    resp1 = parser.parse(msg1)
+    assert resp1[0] == Command("SET", ["mykey", "myvalue"])
+
+    msg2 = b"*3\r\n$3\r\nSET\r\n$3\r\nbar\r\n$3\r\n456\r\n*3\r\n$3\r\nSET\r\n$3\r\nbaz\r\n$3\r\n789\r\n"
+    parser = RespParser()
+    resp2 = parser.parse(msg2)
+    assert resp2[0] == Command("SET", ["bar", "456"])
+    assert resp2[1] == Command("SET", ["baz", "789"])
+    assert len(resp2) == 2
+
+    msg3 = b"+PONG\r\n"
+    parser = RespParser()
+    resp3 = parser.parse(msg3)
+    assert resp3[0] == Command("PONG")
+
+    print("All tests passed")
+
+
+if __name__ == "__main__":
+    run_tests()
