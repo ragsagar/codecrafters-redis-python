4d7b163dc4dfe79f926e9bded9a476d2196c2d7d
Rag Sagar <ragsagar@gmail.com>
2024-05-29 21:37:38 +0400

    Update

--- app/server.py
+++ app/server.py
@@ -102,8 +102,8 @@ class RedisServer:
         return command in ["set", "del"]
 
     def get_rdb_file_contents(self):
-        hex_data = open("sample_file.rdb").read()
-        # hex_data = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+        # hex_data = open("sample_file.rdb").read()
+        hex_data = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
         return hex_data
 
     def initialize_server(self):
