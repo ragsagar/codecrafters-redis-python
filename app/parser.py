3623302806172e2f7a4c4b7b9c4f56f450bd0d3e
Rag Sagar <ragsagar@gmail.com>
2024-05-30 15:29:24 +0400

    Parser v2

--- app/parser.py
+++ app/parser.py
@@ -1,3 +1,5 @@
+import sys
+
 from enum import Enum
 
 
@@ -11,7 +13,78 @@ class RespParser:
     def __init__(self) -> None:
         self.state = self.State.START
 
-    def parse(self, message):
+    def read_number(self, message, cursor):
+        number = 0
+        while message[cursor] != b"\r":
+            number = number * 10 + int(message[cursor])
+            cursor += 1
+        return cursor, number
+
+    def skip_newline(self, message, cursor):
+        while message[cursor] in [b"\n", b"\r"]:
+            cursor += 1
+        return cursor
+
+    def read_command(self, message, cursor, part_length):
+        while message[cursor] != b"$":
+            cursor += 1
+        cursor, command_length = self.read_number(message, cursor + 1)
+        cursor = self.skip_newline(message, cursor)
+        command_string = b"".join(message[cursor : cursor + command_length])
+        cursor += command_length
+        command = Command(command_string.decode(), [])
+        part_length -= 1
+        while part_length > 0:
+            while message[cursor] != b"$":
+                cursor += 1
+            cursor, data_length = self.read_number(message, cursor + 1)
+            cursor = self.skip_newline(message, cursor)
+            data = b"".join(message[cursor : cursor + data_length])
+            cursor += data_length
+            command.add_data(data)
+            part_length -= 1
+        return cursor, command
+
+    def read_pending_bytes(self, message, cursor):
+        data = b""
+        while message[cursor] != b"\r":
+            data += message[cursor]
+            cursor += 1
+        return cursor, data
+
+    def read_bytes(self, message, length, cursor):
+        data = b"".join(message[cursor : cursor + length])
+        cursor += length
+        return cursor, data
+
+    def parse(self, msg):
+        cursor = 0
+        part_length = 0
+        commands = []
+        message = [i.to_bytes(1, sys.byteorder) for i in msg]
+        while cursor < len(message):
+            if message[cursor] == b"*":
+                cursor, part_length = self.read_number(message, cursor + 1)
+                cursor, command = self.read_command(message, cursor, part_length)
+                commands.append(command)
+                cursor += 1
+            elif message[cursor] == b"+":
+                cursor, string = self.read_pending_bytes(message, cursor + 1)
+                commands.append(Command(string.decode()))
+                cursor += 1
+            elif message[cursor] == b"$":
+                cursor, data_length = self.read_number(message, cursor + 1)
+                print("Data length", data_length, len(message))
+                cursor = self.skip_newline(message, cursor)
+                print("Cursor", cursor, message[cursor])
+                cursor, data = self.read_bytes(message, data_length, cursor)
+                print("Parsed Data", data, cursor)
+                commands.append(Command("RDB", [data]))
+            else:
+                cursor += 1
+        return commands
+
+    def parse_old(self, message):
         tokens = message.strip()
         print(tokens)
         message_length = 0
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -7,13 +7,13 @@ class TestRespParser(unittest.TestCase):
     def test_parse_single_set_command(self):
         msg = b"*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n"
         resp = RespParser().parse(msg)
-        self.assertEqual(resp, [Command("SET", ["mykey", "myvalue"])])
+        self.assertEqual(resp, [Command("SET", [b"mykey", b"myvalue"])])
 
     def test_parse_multiple_set_command(self):
         msg = b"*3\r\n$3\r\nSET\r\n$3\r\nbar\r\n$3\r\n456\r\n*3\r\n$3\r\nSET\r\n$3\r\nbaz\r\n$3\r\n789\r\n"
         resp = RespParser().parse(msg)
         self.assertEqual(
-            resp, [Command("SET", ["bar", "456"]), Command("SET", ["baz", "789"])]
+            resp, [Command("SET", [b"bar", b"456"]), Command("SET", [b"baz", b"789"])]
         )
 
     def test_parse_ping_command(self):
@@ -29,11 +29,12 @@ class TestRespParser(unittest.TestCase):
     def test_parse_get_command(self):
         msg = b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n"
         resp = RespParser().parse(msg)
-        self.assertEqual(resp, [Command("GET", ["foo"])])
+        self.assertEqual(resp, [Command("GET", [b"foo"])])
 
     def test_parse_rdb_command(self):
         msg = b"$88\r\nREDIS0011\xfa\tredis-ver\x057.2.0\xfa\nredis-bits\xc0@\xfa\x05ctime\xc2m\x08\xbce\xfa\x08used-mem\xc2\xb0\xc4\x10\x00\xfa\x08aof-base\xc0\x00\xff\xf0n;\xfe\xc0\xffZ\xa2"
         resp = RespParser().parse(msg)
+        print("RDB Resp", resp[0])
         self.assertEqual(
             resp,
             [
@@ -58,6 +59,6 @@ class TestRespParser(unittest.TestCase):
                         b"REDIS0011\xfa\tredis-ver\x057.2.0\xfa\nredis-bits\xc0@\xfa\x05ctime\xc2m\x08\xbce\xfa\x08used-mem\xc2\xb0\xc4\x10\x00\xfa\x08aof-base\xc0\x00\xff\xf0n;\xfe\xc0\xffZ\xa2"
                     ],
                 ),
-                Command("REPLCONF", ["GETACK", "*"]),
+                Command("REPLCONF", [b"GETACK", b"*"]),
             ],
         )
