4ae9753159543ec988215d1cb1ea9a540763b1ff
Rag Sagar <ragsagar@gmail.com>
2024-05-31 14:57:31 +0400

    Update

--- app/server.py
+++ app/server.py
@@ -1,8 +1,6 @@
 import socket
 import selectors
 import types
-import datetime
-import uuid
 from enum import Enum
 from .encoder import Encoder
 from .utils import generate_repl_id
@@ -174,83 +172,3 @@ class RedisServer:
     def run(self):
         self.initialize_server()
         self.handle_loop()
-
-
-class MasterConnectionState(Enum):
-    INITIAL = "initial"
-    WAITING_FOR_PONG = "waiting_for_pong"
-    WAITING_FOR_PORT_RESPONSE = "waiting_for_port_response"
-    WAITING_FOR_CAPA_RESPONSE = "waiting_for_capa_response"
-    WAITING_FOR_FULLRESYNC = "waiting_for_fullresync"
-    WAITING_FOR_FILE = "waiting_for_file"
-    READY = "ready"
-
-
-class MasterHandshakeException(Exception):
-    pass
-
-
-class MasterConnection:
-    state = MasterConnectionState.INITIAL
-    server = None
-    port = None
-    listening_port = None
-    replica_id = "?"
-    offset = -1
-    offset_count = 0
-    sent_ping = False
-
-    def __init__(self, server, listening_port, socket):
-        self.server = server
-        self.listening_port = listening_port
-        self.socket = socket
-        self.encoder = Encoder()
-        self.parser = RespParser()
-        self.command_handler = ClientCommandHandler(server, self)
-
-    def service_connection(self, key, mask):
-        sock = key.fileobj
-        data = key.data
-        if mask & selectors.EVENT_READ:
-            recv_data = sock.recv(1024)
-            if recv_data:
-                data.outb += recv_data
-                self.log("Received", repr(recv_data), "from", data.addr)
-            else:
-                self.log("Closing connection to", data.addr)
-                sel.unregister(sock)
-                sock.close()
-        if mask & selectors.EVENT_WRITE:
-            self.handle_incoming_data(data, sock)
-            data.outb = b""
-
-    def handle_incoming_data(self, data, sock):
-        if not self.sent_ping:
-            print("Sending ping to master")
-            sock.sendall(self.encoder.generate_array_string(["PING"]))
-            self.set_state(MasterConnectionState.WAITING_FOR_PONG)
-        if data.outb:
-            response = self.command_handler.handle_message(data, sock)
-            print("Sending response from slave:", response)
-            if response:
-                sock.sendall(response)
-
-    def set_state(self, state):
-        print(f"Changing state from {self.state} to {state}")
-        self.state = state
-
-    def log(self, message, *args):
-        print("Replica: ", message, *args)
-
-    def set_offset_and_replica(self, offset, replica_id):
-        self.offset = offset
-        self.replica_id = replica_id
-
-    def get_listening_port(self):
-        return self.listening_port
-
-    def get_replica_id(self):
-        return self.replica_id
-
-    def get_offset(self):
-        return self.offset
