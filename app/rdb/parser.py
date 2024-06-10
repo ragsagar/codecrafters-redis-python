8c706893cf08f3348bb9013b28dee2f8826887c6
Rag Sagar <ragsagar@gmail.com>
2024-06-10 10:05:10 +0400

    Fix the parser

--- app/rdb/parser.py
+++ app/rdb/parser.py
@@ -1,19 +1,25 @@
 import sys
 from enum import Enum
+from collections import defaultdict
 
 
 class RdbData:
     version = None
-    db_number = None
 
-    def init(self):
+    def __init__(self):
         self.data = {}
+        self.current_selector = None
 
     def set_version(self, version):
         self.version = version
 
-    def set_db_number(self, db_number):
-        self.db_number = db_number
+    def add_database(self, number):
+        print(self.data, number)
+        self.data[number] = []
+        self.current_selector = number
+
+    def add_key_value(self, kv):
+        self.data[self.current_selector].append(kv)
 
 
 class InvalidRdbFileException(Exception):
@@ -23,16 +29,16 @@ class InvalidRdbFileException(Exception):
 class RdbParser:
     class State(Enum):
         INIT = "init"
-        VALID_RDB = "valid_rdb"
         READ_KEYS = "read_keys"
-        DATA = "data"
         DONE = "done"
 
     state = State.INIT
-    DATABASE_SELECTOR = 0xFD
+    DATABASE_SELECTOR = 0xFE
     MAGIC_BYTES = [ord("R"), ord("E"), ord("D"), ord("I"), ord("S")]
     MILLIS_EXPIRATION = 0xFC
     SECOND_EXPIRATION = 0xFD
+    RDB_FILE_END = 0xFF
+    STRING_TYPE = 0x0
 
     def check_magic_bytes(self, cursor, data):
         for byte in self.MAGIC_BYTES:
@@ -56,13 +62,17 @@ class RdbParser:
 
     def read_db_number(self, cursor, data):
         cursor += 1
-        return cursor + 1, int(data[cursor])
+        print("Reading db number")
+        return self.read_length(cursor, data)
 
     def read_seconds(self, cursor, data):
         return self.read_number_from_bytes(cursor, data, 4)
 
     def read_milliseconds(self, cursor, data):
+        print("Read millis", cursor)
         return self.read_number_from_bytes(cursor, data, 8)
+        # print("Reading milliseconds")
+        # return self.read_length(cursor, data)
 
     def convert_sec_to_milli(self, seconds):
         return seconds * 1000
@@ -75,6 +85,9 @@ class RdbParser:
 
     def read_length(self, cursor, data):
         decision_bits = self.get_two_most_signifcant_bit(data[cursor])
+        print(
+            "decision bits", decision_bits, "cursor", cursor, "data", chr(data[cursor])
+        )
         if decision_bits == 0:
             # Take the next 6 bits as the length
             print("Reading lenght from next 6 bits")
@@ -95,56 +108,89 @@ class RdbParser:
         key = "".join([chr(i) for i in data[cursor : cursor + length]])
         return cursor + length, key
 
-    def read_keys(self, cursor, data):
+    def read_value(self, cursor, data, value_type):
+        if value_type == self.STRING_TYPE:
+            return self.read_string_encoding(cursor, data)
+        return None
+
+    def read_key_value(self, cursor, data):
+        expiry_millis = None
+        kv = KeyValue()
         if data[cursor] == self.SECOND_EXPIRATION:
             cursor += 1
             cursor, seconds = self.read_seconds(cursor, data)
-            milliseconds = self.convert_sec_to_milli(seconds)
+            expiry_millis = self.convert_sec_to_milli(seconds)
+            value_type = data[cursor]
             cursor, key = self.read_string_encoding(cursor, data)
             cursor += 1
             cursor, value = self.read_string_encoding(cursor, data)
-            print("Key:", key, "Value:", value)
+            kv.set_key_value(key, value, value_type)
+            kv.set_expiry_seconds(seconds)
         elif data[cursor] == self.MILLIS_EXPIRATION:
             cursor += 1
-            cursor, milliseconds = self.read_milliseconds(cursor, data)
-            cursor += 1
+            cursor, expiry_millis = self.read_milliseconds(cursor, data)
             value_type = data[cursor]
             cursor += 1
             cursor, key = self.read_string_encoding(cursor, data)
-            cursor += 1
             cursor, value = self.read_string_encoding(cursor, data)
-            print("Key:", key, "Value:", value, "type", value_type)
+            kv.set_key_value(key, value, value_type)
+            kv.set_expiry_milliseconds(expiry_millis)
         else:
-            cursor, key = self.read_string_encoding(cursor, data)
+            value_type = data[cursor]
             cursor += 1
+            cursor, key = self.read_string_encoding(cursor, data)
             cursor, value = self.read_string_encoding(cursor, data)
-            print("Key:", key, "Value:", value)
+            kv.set_key_value(key, value, value_type)
+        return cursor, kv
 
     # data is a bytes class
     def parse(self, data):
         cursor = 0
         # data_bytes = [i.to_bytes(1, sys.byteorder) for i in data]
-
-        rdb_data = RdbData()
+        rdb = RdbData()
         cursor = self.check_magic_bytes(cursor, data)
         cursor, version = self.read_version(cursor, data)
-        print("RDB Version:", version)
-        while cursor < len(data):
+        rdb.set_version(version)
+        print("RDB Version:", version, cursor, data[cursor])
+        state = self.State.INIT
+        while cursor < len(data) and self.state != self.State.DONE:
             if data[cursor] == self.DATABASE_SELECTOR:
                 cursor, db_number = self.read_db_number(cursor, data)
                 print("Database number", db_number)
-                cursor, key_values = self.read_keys(cursor, data)
+                rdb.add_database(db_number)
                 # Read key value pairs
-            cursor += 1
-        return rdb_data
+                state = self.State.READ_KEYS
+            elif data[cursor] == self.RDB_FILE_END:
+                state = self.State.DONE
+                break
+            elif state == self.State.READ_KEYS:
+                print("Reading keys", cursor)
+                cursor, kv = self.read_key_value(cursor, data)
+                rdb.add_key_value(kv)
+                cursor += 1
+        return rdb
 
-    def parse_string(self, data):
-        length = int(data[0])
-        string_data = data[len(str(length)) + 1 : len(str(length)) + 1 + length]
-        return string_data
 
-    def parse_integer(self, data):
-        pass
+class KeyValue:
+    def __init__(self, key=None, value=None, expiry=None):
+        self.key = key
+        self.value = value
+        self.expiry = expiry
+        self.data_type = None
 
-    def get_key_values(self):
-        pass
+    def set_data_type(self, data_type):
+        self.data_type = data_type
+
+    def set_expiry_seconds(self, seconds):
+        self.expiry = self.convert_sec_to_milli(seconds)
+
+    def set_expiry_milliseconds(self, millis):
+        self.expiry = millis
+
+    def convert_sec_to_milli(self, seconds):
+        return seconds * 1000
+
+    def set_key_value(self, key, value, data_type):
+        self.key = key
+        self.value = value
+        self.data_type = data_type
--- tests/rdb_parser_test.py
+++ tests/rdb_parser_test.py
@@ -36,23 +36,17 @@ class RDBParserTest(unittest.TestCase):
     # def test_read_milliseconds(self):
     #     parser = RdbParser()
     #     cursor, milliseconds = parser.read_milliseconds(12, self.sample_rdb_data)
-    #     self.assertEqual(cursor, 20)
     #     self.assertEqual(milliseconds, 1639887072573)
+    #     self.assertEqual(cursor, 20)
 
-    # def test_read_seconds(self):
-    #     parser = RdbParser()
-    #     cursor, seconds = parser.read_seconds(12, self.sample_rdb_data)
-    #     self.assertEqual(cursor, 16)
-    #     self.assertEqual(seconds, 1639887072)
-
-    # def test_read_key_with_expiry(self):
-    #     parser = RdbParser()
-    #     cursor = 11
-    #     cursor, key, value, expiration = parser.read_keys(cursor, self.sample_rdb_data)
-    #     self.assertEqual(cursor, 26)
-    #     self.assertEqual(key, b"expires_ms_precision")
-    #     self.assertEqual(value, b"2022-12-25 10:11:12.573 UTC")
-    #     self.assertEqual(expiration, 0)
+    def test_read_key_with_expiry(self):
+        parser = RdbParser()
+        cursor, kv = parser.read_key_value(11, self.sample_rdb_data)
+        self.assertEqual(kv.key, "expires_ms_precision")
+        self.assertEqual(kv.value, "2022-12-25 10:11:12.573 UTC")
+        self.assertEqual(kv.data_type, 0)
+        # self.assertEqual(kv.expiry, 0)
+        self.assertEqual(cursor, 70)
 
     def test_read_length(self):
         parser = RdbParser()
@@ -65,3 +59,13 @@ class RDBParserTest(unittest.TestCase):
         cursor, value = parser.read_string_encoding(21, self.sample_rdb_data)
         self.assertEqual(value, "expires_ms_precision")
         self.assertEqual(cursor, 42)
+
+    def test_parse(self):
+        parser = RdbParser()
+        rdb = parser.parse(self.sample_rdb_data)
+        self.assertEqual(rdb.version, 4)
+        kv = rdb.data[0][0]
+        self.assertEqual(kv.key, "expires_ms_precision")
+        self.assertEqual(kv.value, "2022-12-25 10:11:12.573 UTC")
+        self.assertEqual(kv.data_type, 0)
+        self.assertEqual(len(rdb.data), 1)
